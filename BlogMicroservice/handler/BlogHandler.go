1af77cb655174f97eae010b363b6b3c6aa3a2ea4
jucavidic <jucavidic3@gmail.com>
2024-03-19 20:52:12 +0100

    Added BlogHandler, BlogService, BlogRepository and CommentRepository

---
+++ BlogMicroservice/handler/BlogHandler.go
@@ -0,0 +1,89 @@
+package handler
+
+import (
+	"database-example/model"
+	"database-example/service"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type BlogHandler struct {
+	BlogService *service.BlogService
+}
+
+func (handler *BlogHandler) GetBlog(writer http.ResponseWriter, req *http.Request) {
+	id := mux.Vars(req)["id"]
+	log.Printf("Blog sa id-em %s", id)
+	blog, err := handler.BlogService.FindBlogById(id)
+	writer.Header().Set("Content-Type", "application/json")
+
+	if err != nil {
+		writer.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	writer.WriteHeader(http.StatusOK)
+	json.NewEncoder(writer).Encode(blog)
+}
+
+func (handler *BlogHandler) GetComment(writer http.ResponseWriter, req *http.Request) {
+	id := mux.Vars(req)["id"]
+	log.Printf("Komentar sa id-em %s", id)
+	comment, err := handler.BlogService.FindCommentById(id)
+	writer.Header().Set("Content-Type", "application/json")
+
+	if err != nil {
+		writer.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	writer.WriteHeader(http.StatusOK)
+	json.NewEncoder(writer).Encode(comment)
+}
+
+func (handler *BlogHandler) CreateBlog(writer http.ResponseWriter, req *http.Request) {
+	var blog model.Blog
+	err := json.NewDecoder(req.Body).Decode(&blog)
+
+	if err != nil {
+		println("Error while parsing json")
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = handler.BlogService.CreateBlog(&blog)
+
+	if err != nil {
+		println("Error while creating a new blog")
+		writer.WriteHeader(http.StatusExpectationFailed)
+		return
+	}
+
+	writer.WriteHeader(http.StatusCreated)
+	writer.Header().Set("Content-Type", "application/json")
+}
+
+func (handler *BlogHandler) CreateComment(writer http.ResponseWriter, req *http.Request) {
+	var comment model.Comment
+	err := json.NewDecoder(req.Body).Decode(&comment)
+
+	if err != nil {
+		println("Error while parsing json")
+		writer.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = handler.BlogService.CreateComment(&comment)
+
+	if err != nil {
+		println("Error while creating a new comment")
+		writer.WriteHeader(http.StatusExpectationFailed)
+		return
+	}
+
+	writer.WriteHeader(http.StatusCreated)
+	writer.Header().Set("Content-Type", "application/json")
+}
--- BlogMicroservice/model/Comment.go
+++ BlogMicroservice/model/Comment.go
@@ -8,12 +8,12 @@ import (
 )
 
 type Comment struct {
-	ID             uuid.UUID `json:"id"`
-	UserID         uuid.UUID `json:"user id" gorm:"type:uuid;foreignKey:UserID"`
-	BlogID         uuid.UUID `json:"blog id" gorm:"type:uuid;foreignKey:BlogID"`
-	PublishDate    time.Time `json:"publish date" gorm:"not null;type:timestamp"`
-	Text           string    `json:"text" gorm:"not null;type:text"`
-	LastChangeDate time.Time `json:"last change date" gorm:"not null;type:timestamp"`
+	ID                   uuid.UUID `json:"id"`
+	UserID               uuid.UUID `json:"user id" gorm:"type:uuid;foreignKey:UserID"`
+	BlogID               uuid.UUID `json:"blog id" gorm:"type:uuid;foreignKey:BlogID"`
+	PublishDate          time.Time `json:"publish date" gorm:"not null;type:timestamp"`
+	Text                 string    `json:"text" gorm:"not null;type:text"`
+	LastModificationDate time.Time `json:"last modification date" gorm:"not null;type:timestamp"`
 }
 
 func (comment *Comment) BeforeCreate(scope *gorm.DB) error {
