216fc8184b854e183d2c1a0e21287bf2a02fb3fe
jucavidic <jucavidic3@gmail.com>
2024-04-25 09:29:00 +0200

    Added ShoppingCart and OrderItem

---
+++ TourMicroservice/models/OrderItem.go
@@ -0,0 +1,13 @@
+package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type OrderItem struct {
+	gorm.Model
+	TourId         uint    `json:"tourId" gorm:"type:uint;foreignKey:TourID"`
+	TourName       string  `json:"tourName" gorm:"type:string"`
+	TourPrice      float64 `json:"tourPrice"`
+	NumberOfPeople uint    `json:"numberOfPeople"`
+}
